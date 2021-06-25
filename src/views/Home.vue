3d4ba9650b6a2788aa022254884ba3fec4417cd2
Matthew Keil <me@matthewkeil.com>
2021-06-24 21:57:18 -0400

    attempting flip card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <FlashCard frontText="front" backText="back" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import FlashCard from "../components/FlashCard.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    FlashCard,
   },
 });
 </script>
