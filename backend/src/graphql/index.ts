791ab24618a1f6b46cdcb8b5480f56d6a68b3fc4
matthewkeil <me@matthewkeil.com>
2022-07-31 01:42:44 -0600

    refactor folders and install deps

---
+++ backend/src/graphql/index.ts
@@ -0,0 +1,34 @@
+import http from "http";
+import express from "express";
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+import { resolvers } from "../models";
+
+async function startApolloServer() {
+  const schema = await buildSchema({
+    resolvers,
+  });
+
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    cache: "bounded",
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
